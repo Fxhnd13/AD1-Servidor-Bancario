d6fe0582411067a8635d8d98545c9cfbd63795bf
Jose Soberanis <jcsru13@gmail.com>
2021-10-11 09:29:44 -0600

    Add: password recovery email

--- source/controllers/authentication_controller.js
+++ source/controllers/authentication_controller.js
@@ -60,7 +60,18 @@ const is_logged_in = (req, res) => {
     });
 };
 
+const generate_password = (length) => {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
+
 module.exports = {
     login,
-    logout
+    logout,
+    generate_password
 }
