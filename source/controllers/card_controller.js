33479e0efabc9d67e057a96bc4cb88a08ff657e1
Jose Soberanis <jcsru13@gmail.com>
2021-10-25 08:50:52 -0600

    Fix: now the card owner verification works fine

--- source/controllers/card_controller.js
+++ source/controllers/card_controller.js
@@ -18,14 +18,20 @@ const card_statement = (req, res) => {
         if(session == null){
             res.status(401).json({information_message: 'Token de sesion ha expirado, inicie sesion nuevamente.'});
         }else{
-            Card.findOne({where: {id_card: req.query.id_card}, raw: true}).then(card =>{
-                if(card.card_type == 1){
-                    credit_card_statement(req, res, session);
-                }else if(card.card_type == 2){
-                    debit_card_statement(req, res, session);
-                }else{
-                    res.status(403).json({information_message: 'No existe el tipo de tarjeta solicitado.'});
-                }
+            Bank_User.findOne({where: {username: session.username}, raw: true}).then(bank_user =>{
+                Card.findOne({where: {id_card: req.query.id_card}, raw: true}).then(card =>{
+                    if((card.cui == bank_user.cui) || (bank_user.user_type > 2)){
+                        if(card.card_type == 1){
+                            credit_card_statement(req, res, session);
+                        }else if(card.card_type == 2){
+                            debit_card_statement(req, res, session);
+                        }else{
+                            res.status(403).json({information_message: 'No existe el tipo de tarjeta solicitado.'});
+                        }
+                    }else{
+                        res.status(403).json({information_message: 'No tiene acceso a esta información, la tarjeta solicitada no le pertenece.'});
+                    }
+                });
             });
         }
     });
@@ -33,26 +39,20 @@ const card_statement = (req, res) => {
 
 const credit_card_statement = (req, res, session) => {
     Credit_Card.findOne({where : {id_card: req.query.id_card}, raw: true}).then(credit_card => {
-        Bank_User.findOne({where: {username: session.username}, raw: true}).then(bank_user =>{
-            if((credit_card.cui == bank_user.cui) || (bank_user.user_type > 2)){
-                Card_Payment_Log.findAll({where: {id_card: credit_card.id_card}, raw: true}).then(payments =>{
-                    Payment_Delay.findAll({where: {id_card: credit_card.id_card}, raw: true}).then(payments_delayed =>{
-                        res.status(200).json({
-                            id_card: credit_card.id_card,
-                            cui: credit_card.cui,
-                            credit_card_type: credit_card.credit_card_type,
-                            credit_limit: credit_card.credit_limit,
-                            interest_rate: credit_card.interest_rate,
-                            cutoff_date: credit_card.cutoff_date,
-                            balance: credit_card.balance,
-                            payments: payments,
-                            payments_delayed: payments_delayed
-                        });
-                    });
+        Card_Payment_Log.findAll({where: {id_card: credit_card.id_card}, raw: true}).then(payments =>{
+            Payment_Delay.findAll({where: {id_card: credit_card.id_card}, raw: true}).then(payments_delayed =>{
+                res.status(200).json({
+                    id_card: credit_card.id_card,
+                    cui: credit_card.cui,
+                    credit_card_type: credit_card.credit_card_type,
+                    credit_limit: credit_card.credit_limit,
+                    interest_rate: credit_card.interest_rate,
+                    cutoff_date: credit_card.cutoff_date,
+                    balance: credit_card.balance,
+                    payments: payments,
+                    payments_delayed: payments_delayed
                 });
-            }else{
-                res.status(403).json({information_message: 'No tiene acceso a esta información, la cuenta no le pertenece.'});
-            }
+            });
         });
     });
 };
@@ -60,19 +60,14 @@ const credit_card_statement = (req, res, session) => {
 const debit_card_statement = (req, res, session) => {
     Debit_Card.findOne({where: {id_card: req.query.id_card}, raw: true}).then(debit_card =>{
         Account.findOne({where: {id_account: debit_card.id_account}, raw: true}).then(account => {
-            Bank_User.findOne({where: {username: session.username}, raw: true}).then(bank_user => {
-                if((account.cui == bank_user.cui) || (bank_user.user_type > 2)){
-                    Card_Payment_Log.findAll({where: {id_card: debit_card.id_card}, raw: true}).then(payments =>{
-                        res.status(200).json({
-                            id_card: debit_card.id_card,
-                            cui: account.cui,
-                            id_account: debit_card.id_account,
-                            payments: payments
-                        });
-                    });
-                }else{
-                    res.status(403).json({information_message: 'No tiene acceso a esta información, la cuenta no le pertenece.'});
-                }
+            Card_Payment_Log.findAll({where: {id_card: debit_card.id_card}, raw: true}).then(payments =>{
+                res.status(200).json({
+                    id_card: debit_card.id_card,
+                    cui: account.cui,
+                    id_account: debit_card.id_account,
+                    balance: account.balance,
+                    payments: payments
+                });
             });
         });
     });
